5744c22f188b199f152eac994a66b45777c391f2
Fedulov Anton, P <live.stone.anton@gmail.com>
2023-04-04 23:26:06 +0500

    refactor, added field types
    closes #5

--- src/builder/form.ts
+++ src/builder/form.ts
@@ -1,23 +1,26 @@
 import { object, ObjectSchema, Schema } from "yup";
-import { FormEntities, isGroup } from "../types";
+import { FormEntity, isGroup } from "../types";
 
 type ValidatorShape = {
   [x: string]: ObjectSchema<{}> | Schema;
 };
 
-function validatorMapper(acc: ValidatorShape, formEnity: FormEntities) {
+function validatorMapper<TForm>(
+  acc: ValidatorShape,
+  formEnity: FormEntity<TForm, any>
+) {
   if (isGroup(formEnity)) {
-    acc[formEnity.name] = object(
-      formEnity.children.reduce(validatorMapper, {})
+    acc[formEnity.name as string] = object(
+      formEnity.children.reduce(validatorMapper<any>, {}) as any
     );
   } else if (formEnity.validator) {
-    acc[formEnity.name] = formEnity.validator;
+    acc[formEnity.name as string] = formEnity.validator;
   }
 
   return acc;
 }
 
-export function makeForm(config: FormEntities[]) {
+export function makeForm<TForm>(...config: FormEntity<TForm, any>[]) {
   // TODO: reduce in reduce greatest hit =D
   const validation = config.reduce(validatorMapper, {});
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,21 @@
 import { Schema } from "yup";
 
+export type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
+export interface FormValues {
+  dateOn: Date;
+  someName: {
+    name: string;
+    patronymic: string;
+    jobTitle: string;
+    address: string;
+  };
+}
+
 export type FieldType = "input" | "select";
 
 export interface Option<TOption = string> {
@@ -7,9 +23,9 @@ export interface Option<TOption = string> {
   value: TOption;
 }
 
-export interface FormField<TForm extends object = {}> {
+export interface FormField<TForm, K extends keyof TForm> {
   //TODO: типизировать name или нет?
-  name: keyof TForm;
+  name: K;
   type: FieldType;
   label: string;
   options?: Option[];
@@ -19,14 +35,17 @@ export interface FormField<TForm extends object = {}> {
   validator?: Schema;
 }
 
-export interface FormGroup {
-  name: string;
-  id?: string;
-  children: FormEntities[];
+export interface FormGroup<TForm, K extends keyof TForm> {
+  name: K;
+  children: FormEntity<TForm[K], any>[];
 }
 
-export type FormEntities = FormField | FormGroup;
+export type FormEntity<TForm, K extends keyof TForm> =
+  | FormField<TForm, K>
+  | FormGroup<TForm, K>;
 
-export function isGroup(entity: FormEntities): entity is FormGroup {
+export function isGroup<TForm, K extends keyof TForm>(
+  entity: FormEntity<TForm, K>
+): entity is FormGroup<TForm, K> {
   return "children" in entity;
 }
